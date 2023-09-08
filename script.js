8bd1e6fa9044003805adb2c8850e3dcb585b7cfc
Ghoustham <javier.aguiht@gmail.com>
2023-09-08 13:18:24 -0300

    The function playerSelection has been created

--- script.js
+++ script.js
@@ -14,17 +14,39 @@ function getComputerChoice (){
     } else if (randomNumber >3 && randomNumber <= 6){
 
         computerSelection = "Paper";
+
     } else {
+
         computerSelection = "Scissors";
     }
     
-    
     return console.log (computerSelection, randomNumber);
-
 }
 
 getComputerChoice();
 
 //CREATE A FUNCTION CALLED playerSelection THAT WILL CHOICE RETURN EITHER ROCK, PAPER, SCISSORS.
+
+function playerSelection (){
+    let playerAnswer = prompt ("Please, choice between: Rock, Paper, Scissors ", "Scissors" );
+    switch (playerAnswer) {
+        case playerAnswer = "Rock":
+            console.log ("You choose the rock");
+            break;
+        case playerAnswer = "Scissors":
+            console.log ("You choose Scissors");
+            break;
+
+        case playerAnswer = "Paper":
+            console.log ("You choose Paper");
+            break;
+    }
+    
+    return console.log (playerAnswer);
+}
+
+playerSelection();
+
+
 // CREATE A FUNCTION THAT TAKE TWO PARAMETERS playerSelection and getComputerChoice AND RETURN A STRING THAT DECLARES WIN OR LOSE.
 // CREATE A FUNCTION CALLED game ALLOW US TO PLAY A 5 ROUND GAMES.   
